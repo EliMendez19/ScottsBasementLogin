064446fd5c354de4a636d72fbdcb65611bdf225e
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-04-20 12:04:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const form = document.getElementById("password-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const password = document.getElementById("password").value;
-  if (password === "TL9162010") {
-    window.location.href = "https://the-archives-hub.vercel.app/";
+  if (password === "EM05191o") {
+    window.location.href = "https://sites.google.com/stu.cfisd.net/elisecretstuff/yhgfde45tg";
   } else {
-    alert("Incorrect password. Please try again.");
+    alert("not the right password lol");
   }
 });
