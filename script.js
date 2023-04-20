332a1bff60c949cd36df4c2ed1d56183b21d8cc7
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-04-20 12:12:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const form = document.getElementById("password-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const password = document.getElementById("password").value;
-  if (password === "EM05191o") {
+  if (password === "EM051910") {
     window.location.href = "https://sites.google.com/stu.cfisd.net/elisecretstuff/yhgfde45tg";
   } else {
     alert("not the right password lol");
