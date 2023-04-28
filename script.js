44380555569876b365dd0b24de89e099f82fa333
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-04-28 15:10:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const form = document.getElementById("password-form");
 const userEmail = 'emen0145@stu.cfisd.net';
 
 // check if the user's email matches a specific email
-if (userEmail === 'edu.cfisd.net') {
+if (userEmail === 'stu.cfisd.net') {
   // if the user's email matches, redirect them to a different site
   window.location.href = 'https://www.example.com';
 }
