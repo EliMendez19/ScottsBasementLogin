4e478772016b8050988ae2880099ba7e314cb682
scottsbasement <100329425+EliMendez19@users.noreply.github.com>
2023-04-30 09:44:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,9 @@ const form = document.getElementById("password-form");
 const userEmail = 'emen0145@stu.cfisd.net';
 
 // check if the user's email matches a specific email
-if (userEmail === 'stu.cfisd.net') {
+if (userEmail === '@stu.cfisd.net') {
   // if the user's email matches, redirect them to a different site
   window.location.href = 'https://www.example.com';
-}
-// Define an array of blacklisted email addresses
-const blacklist = ['emen0145@stu.cfisd.net', 'testing@example.com', 'eztobypass@example.com'];
-
-// Get the email address of the user trying to access the website
-const userEmail = prompt('Please enter your email address to access the website:');
-
-// Check if the user's email address is in the blacklist
-if (blacklist.includes(userEmail)) {
-  alert('Access denied! Your email address is blacklisted.');
-} else {
-  alert('Access granted!');
-  // Allow the user to access the website
-  // ...
 }
 form.addEventListener("submit", (event) => {
   event.preventDefault();
